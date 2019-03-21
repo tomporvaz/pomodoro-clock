f38d1c531c486d70c5332ebc072b3304589f478c
Tom Porvaznik <tomporvaz@gmail.com>
2019-03-20 21:21:39 -0400

    Created a practice function for timer that countsdown using setInterval

--- src/App.js
+++ src/App.js
@@ -3,26 +3,59 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      time: 1500,
+      running: true
+    }
+    this.countdown = this.countdown.bind(this);
+    this.timer = this.timer.bind(this);
+    this.testSetTimeout = this.testSetTimeout.bind(this);
+
+  }
+
+  countdown(){
+   // while(this.state.time > 0){
+      
+    //}
+  }
+
+  timer() {
+    let output = this.state.time - 1;
+    console.log("Subtracted 1");
+    this.setState({
+        time: output
+      })
+  }
+
+
+  //test setTimeout
+  testSetTimeout(){
+    let index = 5;
+    setInterval(()=>this.timer(), 1000);
+
+
+    index--;
+    
+  }
+    
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <Session time={this.state.time} />
+          <button onClick={this.testSetTimeout}>Start/Stop</button>
         </header>
       </div>
     );
   }
+}
+
+function Session (props) {
+  return <h1>{props.time}</h1>;
+
 }
 
 export default App;
