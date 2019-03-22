a7c0d042c7eb453b17d1423c9b18d1bdd88942d2
Tom Porvaznik <tomporvaz@gmail.com>
2019-03-22 18:05:05 -0400

    Updated timer function to run only if running is true.  Added fcc testing module to index.html

--- src/App.js
+++ src/App.js
@@ -6,47 +6,59 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      time: 1500,
-      running: true
+      time: 10,
+      running: false
     }
-    this.countdown = this.countdown.bind(this);
     this.timer = this.timer.bind(this);
-    this.testSetTimeout = this.testSetTimeout.bind(this);
-
+    this.toggleRunning = this.toggleRunning.bind(this);
   }
-
-  countdown(){
-   // while(this.state.time > 0){
-      
-    //}
-  }
-
-  timer() {
-    let output = this.state.time - 1;
-    console.log("Subtracted 1");
-    this.setState({
-        time: output
-      })
-  }
-
 
   //test setTimeout
-  testSetTimeout(){
-    let index = 5;
-    setInterval(()=>this.timer(), 1000);
-
-
-    index--;
+  timer(){
     
+ 
+    const timerID = setInterval(()=>{
+      console.log("this.state.time = " + this.state.time);
+      if(this.state.time > 0 && this.state.running){
+        this.setState({
+          time: this.state.time - 1
+        })
+      } else {
+        clearInterval(timerID)
+      }
+    }, 1000);
   }
+
+  //toggle running
+  toggleRunning(){
+    if(this.state.running){
+      console.log("running: " + this.state.running);
+      this.setState({
+        running: false
+      })
+    } else {
+      console.log("running: " + this.state.running);  
+      this.timer();
+        this.setState({
+          running: true
+        })
+      }
+    }
+
+  
     
 
   render() {
+    let test = ()=>{
+      console.log("Minutes = " + 238/60);
+      console.log("Seconds = " + 238%60);
+    }
+    test();
     return (
       <div className="App">
         <header className="App-header">
           <Session time={this.state.time} />
-          <button onClick={this.testSetTimeout}>Start/Stop</button>
+          <button onClick={this.toggleRunning}>Start/Stop</button>
         </header>
       </div>
     );
