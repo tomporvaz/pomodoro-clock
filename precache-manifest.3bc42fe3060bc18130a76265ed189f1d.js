ac466888a4ca5897f9bd81a91babccfaecf60ec0
tomporvaz <tomporvaz@gmail.com>
2019-04-18 21:30:42 -0400

    Updates

---
+++ precache-manifest.3bc42fe3060bc18130a76265ed189f1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "202bc4ce353be35c5422",
+    "url": "/pomodoro-clock/static/css/main.f77cc40c.chunk.css"
+  },
+  {
+    "revision": "202bc4ce353be35c5422",
+    "url": "/pomodoro-clock/static/js/main.6bd96b1a.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "9b3a5007987034dff77a",
+    "url": "/pomodoro-clock/static/js/2.652dea17.chunk.js"
+  },
+  {
+    "revision": "bdc8f8219d1383e2af4aff6e6953762f",
+    "url": "/pomodoro-clock/static/media/alarm-sound.bdc8f821.mp3"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/pomodoro-clock/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "b99a04c16fb767475becacbdad8275c3",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
